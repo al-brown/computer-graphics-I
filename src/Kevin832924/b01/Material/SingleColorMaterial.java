1ca3079cbf89a479e5d7f1b8dc2392cd0cac6eb1
Ishau Alexander Kevin <s61309@beuth-hochschule.de>
2017-01-24 16:34:31 +0100

    Upload new file

---
+++ src/Kevin832924/b01/Material/SingleColorMaterial.java
@@ -0,0 +1,52 @@
+package kevin832924.b01.Material;
+
+
+import kevin832924.b01.Raytracer.Color;
+import kevin832924.b01.Raytracer.Group;
+import kevin832924.b01.Raytracer.Tracer;
+import kevin832924.b01.Shape.Hit;
+
+/**
+ * represents a single color material
+ *
+ * @author Mr. Kevin
+ */
+public class SingleColorMaterial extends Scattered {
+
+    /**
+     * color of the material
+     */
+    public final Color color;
+
+    /**
+     * instantiates a new single color material
+     *
+     * @param color
+     */
+    public SingleColorMaterial(final Color color) {
+        this.color = color;
+    }
+
+    /**
+     * method that returns the color of a given hit in a given group
+     *
+     * @param hit the Hit object that has to be checked
+     * @param group the group that contains the shape objects of the Hit object
+     * @param tracer
+     * @return the color
+     */
+    @Override
+    public Color colorFor(final Hit hit, final Group group,final Tracer t) {
+        return color;
+    }
+
+    public String toString() {
+        return "SingleColorMaterial{" + "color=" + color + '}';
+    }
+
+	@Override
+	public Color getColor() {
+		return color;
+	}
+
+}
